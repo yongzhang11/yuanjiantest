fbb89a10c3f03816ed40b732b3f3e177c63dc050
yongzhang <ayongzhang@outlook.com>
2024-02-04 14:58:03 +0800

    add login

---
+++ log/test.py
@@ -0,0 +1,4 @@
+from log.login import logint
+
+
+logint.chaungjianfangan(15639799731, 'Zy1996321', "123")
--- test/test.py
+++
@@ -1,5 +0,0 @@
-from test.login import chaungjianfangan
-
-
-if __name__ == '__main__':
-    chaungjianfangan(15639799731, 'Zy1996321', "123")
