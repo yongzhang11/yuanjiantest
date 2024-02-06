2b1e069e8aafef785ca000319d0f6b2c33b95fd9
yongzhang <ayongzhang@outlook.com>
2024-02-06 13:24:48 +0800

    提出创建考试公共方法

--- config/login.py
+++ config/login.py
@@ -1,26 +1,18 @@
 # @Time：2024/2/3 19:30
 # @Author: Allan
 import time
-
-from selenium.webdriver.common.by import By
-
 from config.driver_config import DriverConfig
 
-# import pytest
-
 driver = DriverConfig().driver_config()
+Xpath = driver.find_element_by_xpath
+Id = driver.find_element_by_id
 
+class Public_Methods:
 
-class logint:
-
-    def chaungjianfangan(user, password, fanganmingcheng):
+    def Creating_Solution(user, password, fanganmingcheng):
         driver.maximize_window()
         url = 'https://admin-uat.yuanjian.live/login'
         driver.get(url)
-
-        Xpath = driver.find_element_by_xpath
-        Id = driver.find_element_by_id
-
         Xpath('//*[@id="loginEvent"]/button').click()
         Id('phone').send_keys(user)
         Id('password').send_keys(password)
@@ -36,7 +28,6 @@ class logint:
             for elements in list:
                 if fanganmingcheng in elements:
                     Xpath(f'//a[contains(text(),"{fanganmingcheng}")]').click()
-
         else:
             # 不存在 fanganmingcheng，执行创建项目的操作
             Id('btn_createplan').click()
@@ -45,11 +36,13 @@ class logint:
             Xpath('//*[@id="create_step_2"]/div[2]/div[3]/div/div/div/button').click()
             time.sleep(2)
             Xpath('//*[@id="create_button"]').click()
+
+    def Creath_Exam(exam, name):
         driver.implicitly_wait(10)
         Xpath('//li[@id="nav-main-left-online"][2]').click()
         Xpath('//a[text()= "创建考试" ]').click()
-        Xpath("//h3[text()='线上考试']/../..//button[text()='立即创建']").click()
+        Xpath(f"//h3[text()='{exam}']/../..//button[text()='立即创建']").click()
         driver.implicitly_wait(10)
-        Id('create-plan-name').send_keys(123)
+        Id('create-plan-name').send_keys(name)
         time.sleep(10)
         driver.close()
--- testcase/test.py
+++ testcase/test.py
@@ -1,4 +1,5 @@
-from config.login import logint
+from config.login import Public_Methods
 
 
-logint.chaungjianfangan(15639799731, 'Zy1996321', "123")
+Public_Methods.Creating_Solution(15639799731, 'Zy1996321', "123")
+Public_Methods.Creath_Exam("线上考试", '1234')
