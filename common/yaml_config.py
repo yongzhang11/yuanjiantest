62928259d739420c827c047c0f380284bbed919b
yong.zhang <yong.zhang@netis.com>
2024-03-15 11:15:56 +0800

    添加注释

--- common/yaml_config.py
+++ common/yaml_config.py
@@ -6,14 +6,19 @@ import yaml
 
 class GentConf:
     def __init__(self):
+        # 打开配置文件
         with open("../config/environment.yaml", "r", encoding="utf-8") as env_file:
+            # 加载配置文件内容到self.env变量中
             self.env = yaml.load(env_file, Loader=yaml.FullLoader)
+            # 打印self.env变量内容（可选，已注释掉）
             # print(self.env)
 
     def get_env(self, name):
+        # 从self.env字典中根据name键获取对应的值
         return self.env[name]
 
     def get_env_multipleValued(self, name, i=0):
+        # 获取环境变量中指定名称的列表
         return self.env[name][i]
 
 
