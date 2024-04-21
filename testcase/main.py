27a18d77f1fe3464ba448e40ccc69895fef8cf67
yong.zhang <yong.zhang@netis.com>
2024-04-21 10:29:40 +0800

    解决浏览器多开问题

--- testcase/main.py
+++ testcase/main.py
@@ -1,4 +1,7 @@
 import pytest
 
+from config.public import driver
+
 if __name__ == '__main__':
     pytest.main(['-m login', '--html=../report/report.html'])
+    driver.close()
--- testcase/test_login.py
+++ testcase/test_login.py
@@ -9,10 +9,9 @@ from config.driver_config import DriverConfig
 class Test1:
     @pytest.mark.login
     def test_login1(self):
-        # 获取驱动器配置
-        driver = DriverConfig().driver_config()
         time.sleep(3)
         # 调用登录方法，传入手机号和密码
+
         Public_Methods.Login(GentConf().get_env("username"),
                              GentConf().get_env("password"))
         # 调用创建解决方案方法，传入方案名称
@@ -20,8 +19,6 @@ class Test1:
         # 调用创建考试方法，传入考试名称和方案ID
         Public_Methods.Creath_Exam(GentConf().get_env_multipleValued("ExaminationForm"),
                                    GentConf().get_env("ExaminationTitle"))
-        # 关闭驱动器
-        driver.close()
 
     def test_login2(self):
         # 获取驱动器配置
